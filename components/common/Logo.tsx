6d9d48a3bf073598def0036769a5a614e24c3513
lakshithapramodya <pramodya@onitwayglobal.tech>
2024-10-11 21:34:51 +0530

    fix

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -1,39 +1,41 @@
 import React from "react";
+
 import Image from "next/image";
 import Link from "next/link";
+
 import { cn } from "@/lib/utils";
 
-export default function Logo({
+const Logo = ({
   className,
-  footer = false,
+  footer,
 }: {
   className?: string;
   footer?: boolean;
-}) {
+}) => {
   return (
     <Link href="/" prefetch={false}>
       <Image
         src={footer ? "/images/logo-footer.png" : "/images/logo.png"}
         alt="Logo"
-        width={147}
-        height={33}
+        width={500}
+        height={500}
         className={cn(
-          "shrink-0 transition-transform duration-300 ease-in-out hover:scale-110 dark:hidden",
+          "w-[147px] h-[33px] shrink-0 transition-transform duration-300 ease-in-out hover:scale-110 dark:hidden",
           className
         )}
-        priority
       />
       <Image
-        src="/images/logo-footer.png"
+        src={"/images/logo-footer.png"}
         alt="Logo"
-        width={147}
-        height={33}
+        width={500}
+        height={500}
         className={cn(
-          "shrink-0 transition-transform duration-300 ease-in-out hover:scale-110 hidden dark:block",
+          "w-[147px] h-[33px] shrink-0 transition-transform duration-300 ease-in-out hover:scale-110 hidden dark:block",
           className
         )}
-        priority
       />
     </Link>
   );
-}
+};
+
+export default Logo;
